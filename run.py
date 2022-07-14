d27a9501bb9a8aaeb9f5408a2a53dfff7491d9f4
kcichy37 <kcichy37@gmail.com>
2022-07-14 00:56:06 +0000

    Add type-writing animation to game rules via time and sys import

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import sys
+import time
 import pyfiglet
 import pyinputplus as pyip
 from board import minesweeper
@@ -23,26 +25,30 @@ def main_menu():
         game_rules()
 
         while True:
-            back_to_main = str(input("Enter 'BACK' for main menu:\n")).upper()
+            back_to_main = str(input("\nEnter 'BACK' for main menu:\n")).upper()
 
             if back_to_main == "BACK":
                 main_menu()
                 break
             else:
-                print("\nWrong input.")
+                print(f"\n'{back_to_main}' is the wrong input.")
     elif choice == 2:
         print("\n1) Easy, 5x5 grid with 5 mines")
         print("2) Medium, 10x10 grid with 15 mines")
-        print("3) Hard, 15x15 grid with 35 mines\n")
+        print("3) Hard, 15x15 grid with 35 mines")
+        print("4) Back\n")
 
-        difficulty = pyip.inputInt("Please enter 1 for Easy, 2 for Medium and 3 for Hard:\n", min=1, max=3)
+        difficulty = pyip.inputInt("""Please enter 1 for Easy, 2 for Medium and 3 for Hard 
+        Or 4 to go back to main menu:\n""", min=1, max=4)
 
         if difficulty == 1:
             minesweeper(5, 3)
         elif difficulty == 2:
             minesweeper(10, 10)
-        else:
+        elif difficulty == 3:
             minesweeper(15, 35)
+        else:
+            main_menu()
 
 
 def game_rules():
@@ -51,24 +57,26 @@ def game_rules():
     Explains the game rules to the user
     """
 
-    print("""\n Rules
-        Minesweeper is a game where
-        mines are hidden in a grid of squares.
-        Safe squares have numbers telling you
-        how many mines touch the square.
-        You can use the number clues to solve the
-        game by opening all of the safe squares.
-        If you click on a mine you lose the game!
+    rules = """\n Rules
+    Minesweeper is a game where mines are hidden in a grid.
+    Safe squares have numbers telling you how many mines touch the square.
+    You can use the number clues to solve the game, 
+    by opening all of the safe squares.
+    If you click on a mine you lose the game!
 
-        When you open a square
-        that does not touch any mines,
-        it will be empty and the adjacent
-        squares will automatically open
-        in all directions until reaching
-        squares that contain numbers.
-        A common strategy for starting
-        games is to randomly mark a spot
-        until you get a big opening with lots of numbers.\n""")
+    When you open a square that does not touch any mines,
+    it will be empty and the adjacent squares will automatically open
+    in all directions until reaching squares that contain numbers.
+    A common strategy for starting games is to randomly mark a spot
+    until you get a big opening with lots of number clues."""
+
+    def typewriter(rules):
+        for char in rules:
+            sys.stdout.write(char)
+            sys.stdout.flush()
+            time.sleep(0.02)
+
+    print(typewriter(rules))
 
 
 main_menu()
