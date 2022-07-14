c5c126579278f623cf57e393d370689e6f4e786d
kcichy37 <kcichy37@gmail.com>
2022-07-14 00:00:45 +0000

    Fix mine neighbouring number not displaying for every mine, wrong indentation of if statement

--- board.py
+++ board.py
@@ -11,39 +11,39 @@ def minesweeper(board_size, mines):
     for num in range(mines):
         row = random.randint(0, board_size-1)
         col = random.randint(0, board_size-1)
-        board[col][row] = "X"
+        board[col][row] = "*"
 
-    if (row >= 0 and row <= board_size-2) and (col >= 0 and col <= board_size-1):
-        if board[col][row+1] != "X":
-            board[col][row+1] += 1
+        if (row >= 0 and row <= board_size-2) and (col >= 0 and col <= board_size-1):
+            if board[col][row+1] != "*":
+                board[col][row+1] += 1
 
-    if (row >= 1 and row <= board_size-1) and (col >= 1 and col <= board_size-1):
-        if board[col][row-1] != "X":
-            board[col][row-1] += 1
+        if (row >= 1 and row <= board_size-1) and (col >= 1 and col <= board_size-1):
+            if board[col][row-1] != "*":
+                board[col][row-1] += 1
 
-    if (row >= 1 and row <= board_size-1) and (col >= 1 and col <= board_size-1):
-        if board[col-1][row-1] != "X":
-            board[col-1][row-1] += 1
+        if (row >= 1 and row <= board_size-1) and (col >= 1 and col <= board_size-1):
+            if board[col-1][row-1] != "*":
+                board[col-1][row-1] += 1
 
-    if (row >= 0 and row <= board_size-2) and (col >= 1 and col <= board_size-1):
-        if board[col-1][row+1] != "X":
-            board[col-1][row+1] += 1
+        if (row >= 0 and row <= board_size-2) and (col >= 1 and col <= board_size-1):
+            if board[col-1][row+1] != "*":
+                board[col-1][row+1] += 1
 
-    if (row >= 0 and row <= board_size-1) and (col >= 1 and col <= board_size-1):
-        if board[col-1][row] != "X":
-            board[col-1][row] += 1
+        if (row >= 0 and row <= board_size-1) and (col >= 1 and col <= board_size-1):
+            if board[col-1][row] != "*":
+                board[col-1][row] += 1
 
-    if (row >= 0 and row <= board_size-2) and (col >= 1 and col <= board_size-2):
-        if board[col+1][row+1] != "X":
-            board[col+1][row+1] += 1
+        if (row >= 0 and row <= board_size-2) and (col >= 1 and col <= board_size-2):
+            if board[col+1][row+1] != "*":
+                board[col+1][row+1] += 1
 
-    if (row >= 1 and row <= board_size-1) and (col >= 0 and col <= board_size-2):
-        if board[col+1][row-1] != "X":
-            board[col+1][row-1] += 1
+        if (row >= 1 and row <= board_size-1) and (col >= 0 and col <= board_size-2):
+            if board[col+1][row-1] != "*":
+                board[col+1][row-1] += 1
 
-    if (row >= 0 and row <= board_size-1) and (col >= 0 and col <= board_size-2):
-        if board[col+1][row] != "X":
-            board[col+1][row] += 1
+        if (row >= 0 and row <= board_size-1) and (col >= 0 and col <= board_size-2):
+            if board[col+1][row] != "*":
+                board[col+1][row] += 1
 
     max_row_len = len(max(board, key=len))
     print('    ', end='')
